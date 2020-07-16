5421f4f07c19e6e64bb391f54cba7e3cf7a1357c
Tarique Mirza <sajmtam@gmail.com>
2020-07-17 00:59:33 +0530

    added mongoose and improved code formatting

--- Routers/studentsRouter.js
+++ Routers/studentsRouter.js
@@ -1,39 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-router.get('/', (req, res)=>{
-    res.status(200).json({
-        message: "Here you can GET all Students. ;)"
-    })
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Here you can GET all Students. ;)",
+  });
 });
 
-router.post('/', (req, res)=>{
-    res.status(201).json({
-        message: "Here you can POST a new student. :D"
-    })
+router.post("/", (req, res) => {
+  res.status(201).json({
+    message: "Here you can POST a new student. :D",
+  });
 });
 
-router.get('/:id', (req, res)=>{
-    res.status(200).json({
-        message: `Here you can GET the student with id ${req.params.id}.`
-    })
+router.get("/:id", (req, res) => {
+  res.status(200).json({
+    message: `Here you can GET the student with id ${req.params.id}.`,
+  });
 });
 
-
-router.patch('/:id', (req, res)=>{
-    res.status(200).json({
-        message: `Here you can PATCH the student with id ${req.params.id}.`
-    })
+router.patch("/:id", (req, res) => {
+  res.status(200).json({
+    message: `Here you can PATCH the student with id ${req.params.id}.`,
+  });
 });
 
-
-router.delete('/:id', (req, res)=>{
-    res.status(200).json({
-        message: `Here you can DELETE the student with id ${req.params.id}`
-    })
+router.delete("/:id", (req, res) => {
+  res.status(200).json({
+    message: `Here you can DELETE the student with id ${req.params.id}`,
+  });
 });
-
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,49 +1,62 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const studentRouter = require('./Routers/studentsRouter')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const studentRouter = require("./Routers/studentsRouter");
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Allow CORS
-app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if(req.method==="OPTIONS"){
-        res.header('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE");
-        return res.status(200).json({});
-    }
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    return res.status(200).json({});
+  }
 
-    next();
+  next();
 });
 
 // Add All Routers
-app.use('/students', studentRouter)
-
-
-
+app.use("/students", studentRouter);
 
 // Page not found route - Error 404
-app.use((req, res, next)=>{
-    const error = new Error("Page not found");
-    error.status = 404;
-    next(error);
+app.use((req, res, next) => {
+  const error = new Error("Page not found");
+  error.status = 404;
+  next(error);
 });
 
 // All other error handling
-app.use((error, req, res, next)=>{
-    res.status(error.status || 500).json({
-        error: {
-            message: error.message
-        }
-    })
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
