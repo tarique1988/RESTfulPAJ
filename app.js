5fc852193998580855d238e52fbf03fb1e6adbf1
Tarique Mirza <sajmtam@gmail.com>
2020-07-16 23:18:44 +0530

    Added error handling and page not found handling

--- app.js
+++ app.js
@@ -12,15 +12,38 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// Add Routers
+// Allow CORS
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method==="OPTIONS"){
+        res.header('Access-Control-Allow-Methods', "GET, POST, PATCH, DELETE");
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+// Add All Routers
 app.use('/students', studentRouter)
 
-// Default route
-app.use((req, res)=>{
-    res.status(200).json({
-        message: "Welcome",
-        info: "Our API is under construction!"
-    })
+
+
+
+// Page not found route - Error 404
+app.use((req, res, next)=>{
+    const error = new Error("Page not found");
+    error.status = 404;
+    next(error);
 });
+
+// All other error handling
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
