b37bf497431657c714e83b0974b4a0327c4a7463
Tarique Mirza <sajmtam@gmail.com>
2020-07-18 00:38:47 +0530

    fixed null reference error

--- Controllers/AuthorizationCheck.js
+++ Controllers/AuthorizationCheck.js
@@ -2,14 +2,21 @@ const jwt = require("jsonwebtoken");
 const e = require("express");
 
 module.exports = AuthorizationCheck = (req, res, next) => {
-  console.log(req.headers);
-  const token = req.headers.authorization.split(" ")[1];
-  jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, user) => {
-    if (err) {
-      res.status(500).json({ error: "Unauthorized Access!" });
-    } else {
-      req.body.currentUser = user;
-      next();
-    }
-  });
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, user) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ error: "You're not authorized to view this content!" });
+      } else {
+        req.body.currentUser = user;
+        next();
+      }
+    });
+  } else {
+    res
+      .status(500)
+      .json({ error: "You're not authorized to view this content!" });
+  }
 };
